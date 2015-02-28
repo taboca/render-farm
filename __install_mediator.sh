5ce0118e2b811b999c2aefb884689bc1b2794b56
Marcio <taboca@gmail.com>
2015-02-28 12:08:12 -0300

    fix

--- __install_mediator.sh
+++ __install_mediator.sh
@@ -1,4 +1,4 @@
-apt-get install -y node npm
+apt-get install -y nodejs npm
 git clone https://github.com/taboca/TelaSocial-Mediator.git
 cp -r ./mediator-app ./TelaSocial-Mediator/app
 cd TelaSocial-Mediator
@@ -6,6 +6,8 @@ npm install
 cd app
 cd init.d
 ./__install.sh
+npm install forever -g
+sudo ln -s /usr/bin/nodejs /usr/bin/node
 
 
 
