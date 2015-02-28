45be28d439b79793cb9768788b4b3f6eb13701ea
Marcio <taboca@gmail.com>
2015-02-28 11:42:03 -0300

    attempt to install tela social

---
+++ render_install.sh
@@ -0,0 +1,32 @@
+# dash
+
+## Install Tela Social simple client
+
+git clone git@github.com:taboca/TelaSocial.git
+cd TelaSocial
+cd builder
+./0...
+./1...
+./2...
+cd installer
+./0..
+
+### Will install to /usr/lib/taboca/telasocial (bin)
+
+## Install basic frames to xvfb and avconv
+
+apt-get install -y xvfb imagemagick firefox x11-apps libab-tools 
+
+## Mediator for http
+
+apt-get install -y node npm
+
+git clone https://github.com/taboca/TelaSocial-Mediator.git
+
+cd TelaSocial-Mediator
+
+npm install
+
+
+
+
