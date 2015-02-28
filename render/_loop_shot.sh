595992dd4c018269f72550e9c606ade98aa01bc9
Marcio <taboca@gmail.com>
2015-02-28 12:12:12 -0300

    fix

--- render/_loop_shot.sh
+++ render/_loop_shot.sh
@@ -6,7 +6,7 @@ export DISPLAY=:99
 
 /usr/bin/xwd -root -out file.xwd
 convert file.xwd -resize 640x420 current.png
-cp current.png /opt/eldes/TelaSocial-Mediator/app/static 
+cp current.png /root/dash/TelaSocial-Mediator/app/static 
 
 ((CC=CC+1))
 new=$(printf "%04d" ${CC})
